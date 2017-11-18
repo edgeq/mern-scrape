f975b1a542155f49525d2f02094b2c0b572506f1
Edge Quintanilla <edgarq86@gmail.com>
2017-11-18 10:03:45 -0600

    setup

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
+// Set up promises with mongoose
+mongoose.Promise = global.Promise;
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/nytscrape",
+  {
+    useMongoClient: true
+  }
+);
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
